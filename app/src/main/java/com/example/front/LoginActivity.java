e38dbef71cd73b9336c182f9b8ebade1ffb0b2d8
durumi99 <kms990615@naver.com>
2022-08-19 00:47:44 +0900

    로그인 UI 수정, 제보하기 UI

--- app/src/main/java/com/example/front/LoginActivity.java
+++ app/src/main/java/com/example/front/LoginActivity.java
@@ -5,28 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText edt1, edt2;
-    Button btn1;
+    EditText email, pass;
+    Button submit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edt1 = findViewById(R.id.edt1);
-        edt2 = findViewById(R.id.edt2);
-        btn1 = findViewById(R.id.btn1);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        submit = findViewById(R.id.submit);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = edt1.getText().toString();
-                String password = edt2.getText().toString();
+                String name = email.getText().toString();
+                String password = pass.getText().toString();
                 if(name.equals("") || password.equals("")){
                     Toast.makeText(getApplicationContext(),"사용자 이름과 비밀번호를 입력하세요",Toast.LENGTH_SHORT).show();
                 }
